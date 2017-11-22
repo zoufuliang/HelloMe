0b87b7a4d9f06c607585d4f2c2cefc95549e02d8
zoufuliang <2398145311@qq.com>
2017-11-22 15:12:21 +0800

    添加了一个按钮

--- app/src/main/java/com/example/administrator/hellome/MainActivity.java
+++ app/src/main/java/com/example/administrator/hellome/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.administrator.hellome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int g= 9;
+
+         button = findViewById(R.id.btn);
+
     }
 }
