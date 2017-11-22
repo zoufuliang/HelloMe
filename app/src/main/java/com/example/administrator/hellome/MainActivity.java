72a8c1107ef1e33fd31d241c6685e6fa6b802f1b
zoufuliang <2398145311@qq.com>
2017-11-22 15:19:44 +0800

    添加了一个textview

--- app/src/main/java/com/example/administrator/hellome/MainActivity.java
+++ app/src/main/java/com/example/administrator/hellome/MainActivity.java
@@ -2,14 +2,15 @@ package com.example.administrator.hellome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+   private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textView = findViewById(R.id.text);
     }
 
     @Override
